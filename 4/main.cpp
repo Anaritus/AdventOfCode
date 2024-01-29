f48b4993189b2eecc4aaf5ec60213d1fe3dcaf83
Oleg Sviridov <spe4pg@gmail.com>
2024-01-29 15:21:22 +0300

    added 4 second half

--- 4/main.cpp
+++ 4/main.cpp
@@ -7,10 +7,12 @@ using namespace std;
 
 int getWin(vector<int> first, vector<int> second) {
     int f_i = 0, s_i = 0;
-    int win = 1;
+    int win = 0;
+    sort(first.begin(), first.end());
+    sort(second.begin(), second.end());
     while (f_i < first.size() && s_i < second.size()) {
         if (first[f_i] == second[s_i]) {
-            win <<= 1;
+            win++;
             f_i++;
             s_i++;
         }
@@ -18,7 +20,44 @@ int getWin(vector<int> first, vector<int> second) {
         else if (first[f_i] > second[s_i]) s_i++;
     }
 
-    return win >> 1;
+    return win;
+}
+
+void initNewCard(bool & game, bool & first) {
+    game = false;
+    first = true;
+}
+
+void processCopies(
+        vector<int> & copies, int & card_number,
+        vector<int> & first, vector<int> & second) {
+    int win = getWin(first, second);
+
+    while (copies.size() < card_number + win + 1) {
+        copies.push_back(1);
+    }
+    for (int i = card_number + 1; i < win + card_number + 1; i++) {
+        copies[i] += copies[card_number];
+    }
+
+    card_number++;
+    first.erase(first.begin(), first.end());
+    second.erase(second.begin(), second.end());
+}
+
+int sum(vector<int> v) {
+    int s = 0;
+    for (auto t: v) {
+        s += t;
+    }
+    return s;
+}
+
+void debug_out(vector<int> copies) {
+    for (auto t: copies) {
+        cout << t << " ";
+    }
+    cout << endl;
 }
 
 int main () {
@@ -28,17 +67,13 @@ int main () {
     string s;
     bool game;
     bool first;
-    vector<int> first_cards,second_cards;
-    int answer = 0;
+    vector<int> first_cards,second_cards; // Карты для сравнения
+    vector<int> copies(1, 0); // Копии карточек
+    int card_number = 0;
     while (fin >> s) {
         if (s == "Card") {
-            game = false;
-            first = true;
-            sort(first_cards.begin(), first_cards.end());
-            sort(second_cards.begin(), second_cards.end());
-            answer += getWin(first_cards, second_cards);
-            first_cards.erase(first_cards.begin(), first_cards.end());
-            second_cards.erase(second_cards.begin(), second_cards.end());
+            initNewCard(game, first);
+            processCopies(copies, card_number, first_cards, second_cards);
             continue;
         }
         if (s[s.length() - 1] == ':') {
@@ -52,6 +87,7 @@ int main () {
         if (first) first_cards.push_back(stoi(s));
         else second_cards.push_back(stoi(s));
     }
-    cout << answer << endl;
+    processCopies(copies, card_number, first_cards, second_cards);
+    cout << sum(copies) << endl;
     return 0;
 }
